a5d9c12e58d20642604f7c3e417a82ad25872cd0
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2024-04-07 16:07:43 +0300

    Currency added and plots are functional.

---
+++ WM_TEST_CASE/Assets/Scripts/Defense/BuildManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager main;
+
+    [SerializeField] private GameObject[] towerPrefabs;
+
+    private int selectedTower = 0;
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    public GameObject GetSelectedTower()
+    {
+        return towerPrefabs[selectedTower];
+    }
+}
--- WM_TEST_CASE/Assets/Scripts/Enemy/EnemyHealth.cs
+++ WM_TEST_CASE/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int enemyWorth = 50;
+
+    // this prevents the enemy number becomes below zero.
+    private bool isDestroyed = false;
 
     public void TakeDamage(int damage)
     {
         hitPoints -= damage;
 
-        if (hitPoints <= 0)
+        if (hitPoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(enemyWorth);
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
